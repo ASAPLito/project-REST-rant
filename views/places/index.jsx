19f09705b48a71ab0d7191bce05d3c5e5e9956d8
ASAPLito <micbraner@gmail.com>
2022-06-15 13:39:57 -0400

    Fixed error. Places webpage displays correctly

--- views/places/index.jsx
+++ views/places/index.jsx
@@ -1,11 +1,11 @@
 const React = require('react');
-const Def = require('./default');
+const Def = require('../default');
 
 const index = () => {
     return (
         <Def>
             <main>
-                <h1>Places</h1>
+                <h1>Index Page</h1>
                 <p>This is the places index page.</p>
             </main>
         </Def>
