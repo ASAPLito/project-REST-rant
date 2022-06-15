6e1e0b9e95c762e24dc36bcabfe0576a2955c888
ASAPLito <micbraner@gmail.com>
2022-06-15 13:25:33 -0400

    Cannot get views/places/index.jsx webpage to render

---
+++ views/places/index.jsx
@@ -0,0 +1,15 @@
+const React = require('react');
+const Def = require('./default');
+
+const index = () => {
+    return (
+        <Def>
+            <main>
+                <h1>Places</h1>
+                <p>This is the places index page.</p>
+            </main>
+        </Def>
+    )
+}
+
+module.exports = index;
