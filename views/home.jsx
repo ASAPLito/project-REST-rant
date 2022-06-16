0360ccf4173151350e7a70bedd43a17e5a038bc7
ASAPLito <micbraner@gmail.com>
2022-06-15 20:14:17 -0400

    Added CSS styling to layout page

--- views/home.jsx
+++ views/home.jsx
@@ -7,6 +7,7 @@ const home = () => {
             <main>
                 <h1>HOME</h1>
             </main>
+            <a href="/places"><button className="btn-primary">Places Page</button></a>
         </Def>
     )
 }
