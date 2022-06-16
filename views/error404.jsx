e13ab9f12a271e28ef07ff7406fbc1ea920dfa80
ASAPLito <micbraner@gmail.com>
2022-06-15 21:27:13 -0400

    Added Bootstrap grid to index page

--- views/error404.jsx
+++ views/error404.jsx
@@ -7,6 +7,12 @@ const error404 = () => {
             <main>
                 <h1>404: PAGE NOT FOUND</h1>
                 <p>Oops, sorry, we can't find this page!</p>
+                <div>
+                    <img src="/images/curious-cat.jpg" alt="A cat leaning out and looking at camera" height="500"/>
+                    <div>
+                        Photo by <a href="https://unsplash.com/@bogdanf">Bogdan Farca</a> on <a href="https://unsplash.com/photos/CEx86maLUSc">Unsplash</a>
+                    </div>
+                </div>
             </main>
         </Def>
     )
--- views/home.jsx
+++ views/home.jsx
@@ -6,8 +6,14 @@ const home = () => {
         <Def>
             <main>
                 <h1>HOME</h1>
+                <div>
+                    <img src="/images/cake-slice.jpg" alt="A slice of cake" height="500"/>
+                    <div>
+                        Photo by <a href="https://unsplash.com/@anna_tukhfatullina">Anna Tukhfatullina</a> on <a href="https://unsplash.com/photos/Mzy-OjtCI70">Unsplash</a>
+                    </div>
+                </div>
+                <a href="/places"><button className="btn-primary">Places Page</button></a>
             </main>
-            <a href="/places"><button className="btn-primary">Places Page</button></a>
         </Def>
     )
 }
