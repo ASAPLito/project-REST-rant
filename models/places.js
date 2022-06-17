6c8730fb3913f59ecee92719489085b15c2e16f6
ASAPLito <micbraner@gmail.com>
2022-06-17 12:24:04 -0400

    New places can be added

---
+++ models/places.js
@@ -0,0 +1,13 @@
+module.exports = [{
+    name: 'H-Thai-ML',
+    city: 'Seattle',
+    state: 'WA',
+    cuisines: 'Thai, Pan-Asian',
+    pic: '/images/restaurant-interior.jpg'
+}, {
+    name: 'Coding Cat Cafe',
+    city: 'Pheonix',
+    state: 'AZ',
+    cuisines: 'Coffee, Bakery',
+    pic: '/images/cafe.jpg'
+}];
