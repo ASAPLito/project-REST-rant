3ed23e6c5e38d4f3743e5a501560b6ec383d1d13
ASAPLito <micbraner@gmail.com>
2022-06-24 16:20:07 -0400

    Created database and index route and functionality

--- controllers/places.js
+++ controllers/places.js
@@ -1,110 +1,55 @@
-const router = require('express').Router();
-const places = require('../models/places.js');
+const router = require('express').Router()
+const db = require('../models')
 
 router.get('/', (req, res) => {
-    res.render('places/index', {places})
-});
+    db.Place.find()
+    .then((places) => {
+      res.render('places/index', { places })
+    })
+    .catch(err => {
+      console.log(err) 
+      res.render('error404')
+    })
+})
 
 router.post('/', (req, res) => {
-    console.log(req.body)
-    if (!req.body.pic) {
-        //Default image if one is not provided
-        req.body.pic = 'http://placekitten.com/400/400'
-    }
-    if (!req.body.city) {
-        req.body.city = 'Anytown'
-    }
-    if (!req.body.state) {
-        req.body.state = 'USA'
-    }
-    places.push(req.body)
-    res.redirect('/places')
-});
+  db.Place.create(req.body)
+  .then(() => {
+      res.redirect('/places')
+  })
+  .catch(err => {
+      console.log('err', err)
+      res.render('error404')
+  })
+})
+
+
+router.get('/new', (req, res) => {
+  res.render('places/new')
+})
 
 router.get('/:id', (req, res) => {
-    let id = Number(req.params.id)
-    if (isNaN(id)) {
-        console.log("NaN");
-      res.render('error404')
-    }
-    else if (!places[id]) {
-        console.log("array");
-      res.render('error404')
-    }
-    else {
-        res.render('places/show', { place: places[id], id })
-    }
-  })
-  
-router.delete('/:id', (req, res) => {
-    let id = Number(req.params.id)
-    if (isNaN(id)) {
-      res.render('error404')
-      console.log("delete NaN")
-    }
-    else if (!places[id]) {
-      res.render('error404')
-      console.log("delete array does not exist")
-    }
-    else {
-      places.splice(id, 1)
-      console.log("delete redirect")
-      res.redirect('/places')
-    }
-})
-  
-router.get('/new', (req, res) => {
-    res.render('places/new')
-});
-
-//EDIT ROUTE
-router.get('/:id/edit', (req, res) => {
-  let id = Number(req.params.id)
-  if (isNaN(id)) {
-      res.render('error404')
-  }
-  else if (!places[id]) {
-      res.render('error404')
-  }
-  else {
-    res.render('places/edit', { place: { ...places[id], id } })
-  }
+  res.send('GET /places/:id stub')
 })
 
-//PUT ROUTE
 router.put('/:id', (req, res) => {
-  const id = Number(req.params.id)
-  console.log("This is ID: " + req.params.id);
-  if (isNaN(id)) {
-      res.render('error404')
-      console.log("error is here at spot 1");
-      //This is where my code is getting stuck.
-      //This is the log statement that gets read.
-  }
-  else if (!places[id]) {
-      res.render('error404')
-      console.log("error is here at spot 2")
-  }
-  else {
-      // Dig into req.body and make sure data is valid
-      if (!req.body.pic) {
-          // Default image if one is not provided
-          req.body.pic = 'http://placekitten.com/400/400'
-      }
-      if (!req.body.city) {
-          req.body.city = 'Anytown'
-      }
-      if (!req.body.state) {
-          req.body.state = 'USA'
-      }
-
-      // Save the new data into places[id]
-      places[id] = req.body
-      res.redirect(`/places/${id}`)
-      console.log("edit page is working")
-  }
+  res.send('PUT /places/:id stub')
 })
 
+router.delete('/:id', (req, res) => {
+  res.send('DELETE /places/:id stub')
+})
 
+router.get('/:id/edit', (req, res) => {
+  res.send('GET edit form stub')
+})
 
-module.exports = router;
+router.post('/:id/rant', (req, res) => {
+  res.send('GET /places/:id/rant stub')
+})
+
+router.delete('/:id/rant/:rantId', (req, res) => {
+    res.send('GET /places/:id/rant/:rantId stub')
+})
+
+module.exports = router
