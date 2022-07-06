caf867ad688f32d408c81ffc22366df87ebb1d6b
ASAPLito <micbraner@gmail.com>
2022-07-06 14:18:10 -0400

    Edit, delete, and show routes malfunctioning

--- views/places/edit.jsx
+++ views/places/edit.jsx
@@ -6,7 +6,7 @@ const EditForm = ({ place }) => {
         <Def>
           <main>
             <h1>Edit Place</h1>
-            <form method="POST" action={`/places/${place.id}?_method=PUT`}>
+            <form method="POST" action={`/places/${data.place.id}?_method=PUT`}>
                 <div className="row">
                     <div className="form-group col-sm-6">
                         <label htmlFor="name">Place Name</label>
@@ -17,6 +17,14 @@ const EditForm = ({ place }) => {
                         <input className="form-control" id="pic" name="pic" defaultValue={place.pic} />
                     </div>
                 </div>
+                    <div className='Form-group col-sm-4'>
+                        <label htmlFor='founded'>Founded</label>
+                        <input className='form-control'
+                        id='founded'
+                        name='founded'
+                        value={data.place.founded}
+                        />
+                    </div>
                     <div className="form-group">
                         <label htmlFor="city">City</label>
                         <input className="form-control" id="city" name="city" defaultValue={place.city} />
